97d6317de8d3db5e9b33478e55dbf40d9b6201de
amithsulakhe <amithsulakhe729@gmail.com>
2026-01-01 18:48:00 +0530

    Update root GET route response message to reflect new application name in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
 app.get('/', (req, res) => {
-  return res.json({ message: "Welocome to Expense Tracker App" })
+  return res.json({ message: "Welocome to Start Photography App" })
 });
 
 // Connect to MongoDB
