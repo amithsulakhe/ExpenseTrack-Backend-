86a0237a7a13f6cb35ffbcc1da5d58ab46555884
amithsulakhe <amithsulakhe729@gmail.com>
2026-01-10 10:29:58 +0530

    Update root GET route response message for clarity in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
 app.get('/', (req, res) => {
-  return res.json({ message: "Welocome to Start Photography Web App" })
+  return res.json({ message: "Welocome to Start  Web App" })
 });
 
 app.get("/error", (req, res) => {
