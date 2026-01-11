146ee1cc1f15d921608ac36abd57033c860504f9
amithsulakhe <amithsulakhe729@gmail.com>
2026-01-11 11:49:56 +0530

    Update root GET route response message to reflect new application name in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
 app.get('/', (req, res) => {
-  return res.json({ message: "Welocome to Start Web App" })
+  return res.json({ message: "Welocome to ExpenseTrack Backend" })
 });
 
 app.get("/error", (req, res) => {
